42709b92cf8c9de4423e04548562f04988ac5567
Pascal Bardeau <pascalbardeau@MacBook-Pro-de-Pascal.local>
2017-10-06 13:22:44 +0200

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdButtonModule, MdCheckboxModule, MdToolbarModule} from '@angular/material';
+
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdCheckboxModule,
+    MdToolbarModule
+],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
