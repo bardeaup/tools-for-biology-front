aede5bbc8ef420c74913ac4c663742c02dc14cc5
Pascal Bardeau <pascalbardeau@macbook-pro-de-pascal.local>
2017-11-14 00:36:26 +0100

    WIP : Login page and header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MdButtonModule, MdCheckboxModule, MdToolbarModule} from '@angular/material';
+import {MatButtonModule, MatCardModule, MatCheckboxModule, MatIconModule,
+  MatToolbarModule,MatFormFieldModule, MatMenuModule} from '@angular/material';
 
-
+import {LoginComponent} from '../login/login.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MdButtonModule,
-    MdCheckboxModule,
-    MdToolbarModule
+    MatButtonModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule
 ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/login/login.component.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by pascalbardeau on 13/11/2017.
+ */
+
+import {Component} from "@angular/core";
+
+@Component({
+  selector : 'login',
+  templateUrl: './login.html',
+  styleUrls: ['./login.component.css']
+
+})
+
+export class LoginComponent{}
